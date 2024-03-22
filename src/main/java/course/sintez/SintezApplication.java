1915f7f516dfa5c84689c58a83b69bf9acf4e44d
Khal01 <aliyev_56@list.ru>
2024-03-22 11:46:20 +0300

    start

---
+++ src/main/java/course/sintez/SintezApplication.java
@@ -0,0 +1,13 @@
+package course.sintez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintezApplication.class, args);
+	}
+
+}
